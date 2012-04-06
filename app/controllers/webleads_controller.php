6998eeabee79bf5390bb697f35ab90c84a5ccc6f
Sandeep Srivastava <sandeep@netsetsoftware.com>
2012-04-06 14:14:55 +0530

    revoking testing changes

--- app/controllers/webleads_controller.php
+++ app/controllers/webleads_controller.php
@@ -47,7 +47,6 @@ class WebleadsController extends AppController
     * @Return: none 
     **/
     function beforeFilter(){
-	echo "sandeep testing"	;	die;
 		if(!empty($this->params['prefix']) && $this->params['prefix'] == "admin"){
 			$this->checkUserSession();
 		}else{
